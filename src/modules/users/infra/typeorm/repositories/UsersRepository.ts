db93a4f28cbc19024727dcec6d292bd1bfb21f6d
Arthur Fernandes <arthurbpfernandes@gmail.com>
2020-08-31 19:42:13 -0300

    Corrected typos

--- src/modules/users/infra/typeorm/repositories/UsersRepository.ts
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -1,7 +1,7 @@
 import { getRepository, Repository } from 'typeorm';
 
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
-import ICreateAppointmentDTO from '@modules/users/dtos/ICreateUserDTO';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import User from '../entities/User';
 
@@ -24,12 +24,12 @@ class UsersRepository implements IUsersRepository {
     return user;
   }
 
-  public async create(userData: ICreateAppointmentDTO): Promise<User> {
-    const appointment = this.ormRepository.create(userData);
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = this.ormRepository.create(userData);
 
-    await this.ormRepository.save(appointment);
+    await this.ormRepository.save(user);
 
-    return appointment;
+    return user;
   }
 
   public async save(user: User): Promise<User> {
