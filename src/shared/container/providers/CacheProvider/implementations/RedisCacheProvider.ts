c8791568a062019f0e6fda816a821e9cfd0a3f69
Arthur Fernandes <arthurbpfernandes@gmail.com>
2020-10-12 19:57:46 -0300

    Added an eslint-disable

--- src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -9,6 +9,7 @@ export default class RedisCacheProvider implements ICacheProvider {
     this.client = new Redis(cacheConfig.config.redis);
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   public async save(key: string, value: any): Promise<void> {
     await this.client.set(key, JSON.stringify(value));
   }
