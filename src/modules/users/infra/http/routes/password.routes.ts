7dcbb2afded8d6c8c38aab6be1ea436b3f880171
Arthur Fernandes <arthurbpfernandes@gmail.com>
2020-10-29 12:45:03 -0300

    Fixed celebrate validation (was QUERY, now BODY)

--- src/modules/users/infra/http/routes/password.routes.ts
+++ src/modules/users/infra/http/routes/password.routes.ts
@@ -17,13 +17,14 @@ passwordRouter.post(
   }),
   forgotPasswordController.create,
 );
+
 passwordRouter.post(
   '/reset',
   celebrate({
-    [Segments.PARAMS]: {
-      token: Joi.string().uuid().required(),
+    [Segments.BODY]: {
       password: Joi.string().required(),
       password_confirmation: Joi.string().required().valid(Joi.ref('password')),
+      token: Joi.string().uuid().required(),
     },
   }),
   resetPasswordController.create,
