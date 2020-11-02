97799c664ffc680e03f6173c7b47fa3984589ad8
Arthur Fernandes <arthurbpfernandes@gmail.com>
2020-11-02 10:38:51 -0300

    Fixed user_id not being recorded on appointments table

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -70,10 +70,12 @@ class AppointmentsRepository implements IAppointmentsRepository {
 
   public async create({
     provider_id,
+    user_id,
     date,
   }: ICreateAppointmentDTO): Promise<Appointment> {
     const appointment = this.ormRepository.create({
       provider_id,
+      user_id,
       date,
     });
 
